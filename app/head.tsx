c00412d98c58d2d2dc5f64dc63f88b746f942954
HiroMinase <lostsymbol612@gmail.com>
2023-03-05 02:04:06 +0900

    meta description を追加

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
 		<>
 			<title>minasehiro</title>
 			<meta content="width=device-width, initial-scale=1" name="viewport" />
+			<meta name="description" content="web / iOS / Android エンジニア、写真家として活動する水瀬ひろの作品をまとめてあります。" />
 			<link rel="icon" href="/favicon.ico" />
 		</>
 	)
